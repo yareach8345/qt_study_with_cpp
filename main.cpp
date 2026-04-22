4ebf6a44ad6efc2191c4d237851548d68e5b4599
yareach <mine8345@naver.com>
2026-04-22 16:46:00 +0900

    feature - set window icon

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
-#include <QApplication>
-#include <QWidget>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
+#include <QtGui/QIcon>
+#include <QDirIterator>
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
 
+    QIcon icon(":/images/profile.jpg");
+    if (icon.isNull()) {
+        throw std::runtime_error("icon is null");
+    }
+
     QWidget w;
     w.setWindowTitle("Hello, Qt6!");
-    w.move(300, 300);
-    w.resize(400, 400);
+    w.setGeometry(300, 300, 400, 400);
+    w.setWindowIcon(icon);
     w.show();
 
     return QApplication::exec();
