15204d3e5dbc0292a51bed487bfe1c295906b0f0
yareach <mine8345@naver.com>
2026-04-27 17:39:05 +0900

    chore: add toolBar (wikidocs 3-7)

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <QtWidgets/QStatusBar>
 #include <QDirIterator>
 #include <QMenuBar>
+#include <QToolBar>
+#include <QtGui/QScreen>
 
 #include "widgets/RoundButton.h"
 
@@ -11,6 +13,12 @@ int main(int argc, char* argv[]) {
 
     const QIcon icon(":/images/profile.jpg");
     if (icon.isNull()) {
+        throw std::runtime_error("icon is null");
+    }
+
+    const QIcon o_icon(":/images/circle.png");
+    const QIcon x_icon(":/images/x.png");
+    if (o_icon.isNull() || x_icon.isNull()) {
         throw std::runtime_error("icon is null");
     }
 
@@ -32,17 +40,18 @@ int main(int argc, char* argv[]) {
     btn.setToolTip("This is a <b>TOOL TIP!</b>");
 
     // exit action
-    const auto exit_action = std::make_unique<QAction>("Exit");
+    const auto exit_action = std::make_unique<QAction>(QIcon(":/images/exit.png"), "Exit");
     exit_action->setShortcut(QKeySequence("Ctrl+Q"));
     exit_action->setStatusTip("Exit Application");
     QObject::connect(exit_action.get(), &QAction::triggered, QApplication::quit);
 
     // toggle movable action
-    const auto toggle_movable = std::make_unique<QAction>("Toggle movable");
+    const auto toggle_movable = std::make_unique<QAction>(QIcon(":/images/circle.png"), "Toggle movable");
     toggle_movable->setShortcut(QKeySequence("Ctrl+M"));
     toggle_movable->setStatusTip("Toggle movable");
-    QObject::connect(toggle_movable.get(), &QAction::triggered, [&btn, &statusBar] {
+    QObject::connect(toggle_movable.get(), &QAction::triggered, [&btn, &statusBar, &toggle_movable, &o_icon, &x_icon] {
         const bool new_value = btn.toggle_movable();
+        toggle_movable->setIcon(new_value ? o_icon : x_icon);
         const auto event_message = QString("movable is changed to %1").arg(new_value ? "true" : "false");
         qDebug() << event_message;
         statusBar->showMessage(event_message, 5000);
@@ -54,6 +63,12 @@ int main(int argc, char* argv[]) {
     menu->addAction(exit_action.get());
     menu->addSeparator();
     menu->addAction(toggle_movable.get());
+
+    // tool bar
+    const auto toolBar = std::make_unique<QToolBar>();
+    toolBar->addAction(exit_action.get());
+    toolBar->addAction(toggle_movable.get());
+    w.addToolBar(Qt::LeftToolBarArea, toolBar.get());
 
     w.setWindowTitle("Hello, Qt6!");
     w.setGeometry(300, 300, 400, 400);
