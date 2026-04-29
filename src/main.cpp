ec03e5eee3f27647e7f1e464bfc5737477a2a192
yareach <mine8345@naver.com>
2026-04-29 17:27:03 +0900

    feature: used style with qss (wikidocs 3-10)

--- src/main.cpp
+++ src/main.cpp
@@ -1,102 +1,46 @@
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QMainWindow>
-#include <QtWidgets/QStatusBar>
-#include <QDirIterator>
-#include <QMenuBar>
-#include <QToolBar>
-#include <QtGui/QScreen>
-#include <QtCore/QDate>
-#include <QtCore/QTime>
-#include <QtCore/QDateTime>
-
-#include <iostream>
-
-#include "widgets/RoundButton.h"
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QLabel>
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
 
-    const QIcon icon(":/images/profile.jpg");
-    if (icon.isNull()) {
-        throw std::runtime_error("icon is null");
-    }
-
-    const QIcon o_icon(":/images/circle.png");
-    const QIcon x_icon(":/images/x.png");
-    if (o_icon.isNull() || x_icon.isNull()) {
-        throw std::runtime_error("icon is null");
-    }
-
-    QMainWindow w;
+    QWidget w;
     w.setToolTip("This is a <h1>tool tip</h1> of widget");
+    // style
+    QVBoxLayout layout;
+    w.setLayout(&layout);
 
-    auto btn = RoundButton("Quit", &w);
-    QObject::connect(&btn, &QPushButton::clicked, [] () {
-        QApplication::quit();
-    });
+    QLabel red("Red");
+    red.setStyleSheet(
+        "color: red;"
+        "border-style: solid;"
+        "border-width: 2px;"
+        "border-color: #FA8072;"
+        "border-radius: 3px;"
+    );
+    layout.addWidget(&red);
 
-    QStatusBar *statusBar = w.statusBar();
+    QLabel green("Green");
+    green.setStyleSheet(
+        "color: green;"
+        "background-color: #7FFFD4;"
+    );
+    layout.addWidget(&green);
 
-    QObject::connect(&btn, &RoundButton::moved, [&statusBar](const unsigned int step) {
-        qDebug() << step;
-        statusBar->showMessage(QString("step %1").arg(step));
-    });
+    QLabel blue("Blue");
+    blue.setStyleSheet(
+        "color: blue;"
+        "background-color: #87CEFA;"
+        "border-style: solid;"
+        "border-width: 10px;"
+        "border-color: #FA8072;"
+        "border-radius: 10px;"
+    );
+    layout.addWidget(&blue);
 
-    btn.setToolTip("This is a <b>TOOL TIP!</b>");
-
-    // exit action
-    const auto exit_action = std::make_unique<QAction>(QIcon(":/images/exit.png"), "Exit");
-    exit_action->setShortcut(QKeySequence("Ctrl+Q"));
-    exit_action->setStatusTip("Exit Application");
-    QObject::connect(exit_action.get(), &QAction::triggered, QApplication::quit);
-
-    // toggle movable action
-    const auto toggle_movable = std::make_unique<QAction>(QIcon(":/images/circle.png"), "Toggle movable");
-    toggle_movable->setShortcut(QKeySequence("Ctrl+M"));
-    toggle_movable->setStatusTip("Toggle movable");
-    QObject::connect(toggle_movable.get(), &QAction::triggered, [&btn, &statusBar, &toggle_movable, &o_icon, &x_icon] {
-        const bool new_value = btn.toggle_movable();
-        toggle_movable->setIcon(new_value ? o_icon : x_icon);
-        const auto event_message = QString("movable is changed to %1").arg(new_value ? "true" : "false");
-        qDebug() << event_message;
-        statusBar->showMessage(event_message, 5000);
-    });
-
-    // menu bar
-    const auto menu_bar = w.menuBar();
-    QMenu *menu = menu_bar->addMenu("menu");
-    menu->addAction(exit_action.get());
-    menu->addSeparator();
-    menu->addAction(toggle_movable.get());
-
-    // tool bar
-    const auto toolBar = std::make_unique<QToolBar>();
-    toolBar->addAction(exit_action.get());
-    toolBar->addAction(toggle_movable.get());
-    w.addToolBar(Qt::LeftToolBarArea, toolBar.get());
-
-    w.setWindowTitle("Hello, Qt6!");
-    w.setGeometry(600, 600, 400, 400);
-    w.setWindowIcon(icon);
+    w.resize(500 , 500);
     w.show();
-
-    auto srcRect = QGuiApplication::primaryScreen()->availableGeometry();
-    auto center = w.geometry();
-    center.moveCenter(srcRect.center());
-    w.setGeometry(center);
-
-    // print current time (3-9)
-    const QDate date = QDate::currentDate();
-    const QTime time = QTime::currentTime();
-    const QDateTime datetime = QDateTime::currentDateTime();
-
-    std::cout << date.toString(Qt::ISODate).toStdString() << std::endl;
-    std::cout << time.toString(Qt::ISODate).toStdString() << std::endl;
-    std::cout << datetime.toString(Qt::ISODate).toStdString() << std::endl;
-
-    std::cout << date.toString("yyyy-MM-dd dddd").toStdString() << std::endl;
-    std::cout <<time.toString("hh:mm:ss").toStdString() << std::endl;
-    std::cout <<datetime.toString("yyyy-MM-dd ddd hh:mm:ss").toStdString() << std::endl;
 
     return QApplication::exec();
 }
