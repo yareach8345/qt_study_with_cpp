dc30808effe3d027ac8be5dcc1a797d45c1adbbd
yareach <mine8345@naver.com>
2026-05-02 23:30:53 +0900

    practice: grid Layout (wikidocs 4-3)

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,79 @@
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QWidget>
 #include <QtWidgets/QHBoxLayout>
-#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QTextEdit>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QMessageBox>
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
 
     QWidget w;
-
     QVBoxLayout vbox;
-    QHBoxLayout hbox;
-
-    QPushButton ok_btn("ok");
-    ok_btn.setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
-    QPushButton cancel_btn("cancel");
-    cancel_btn.setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
-
     w.setLayout(&vbox);
-    vbox.addStretch(2);
-    vbox.addLayout(&hbox, 1);
-    vbox.addStretch(1);
 
-    hbox.addStretch(1);
-    hbox.addWidget(&ok_btn, 1);
-    hbox.addWidget(&cancel_btn, 2);
-    hbox.addStretch(1);
+    QGridLayout grid;
+    vbox.addLayout(&grid);
+
+    QLabel title_label("Title: ");
+    grid.addWidget(&title_label, 0, 0);
+    QLabel author_label("Author: ");
+    grid.addWidget(&author_label, 1, 0);
+    QLabel review_label("Review: ");
+    grid.addWidget(&review_label, 2, 0);
+
+    QLineEdit title_input;
+    grid.addWidget(&title_input, 0, 1);
+    QLineEdit author_input;
+    grid.addWidget(&author_input, 1, 1);
+    QTextEdit review_input;
+    grid.addWidget(&review_input, 2, 1);
+
+    QHBoxLayout hbox;
+    vbox.addLayout(&hbox);
+
+    auto clear_inputs = [&] {
+        title_input.clear();
+        author_input.clear();
+        review_input.clear();
+    };
+
+    const QString log_form("submit - title : %0, author: %1, review: %2");
+
+    QPushButton submit_button("Submit");
+    hbox.addWidget(&submit_button, 1);
+    QObject::connect(&submit_button, &QAbstractButton::clicked, [&] {
+        const auto title = title_input.text();
+        const auto author = author_input.text();
+        const auto review = review_input.toPlainText();
+
+        QMessageBox msgBox;
+
+        if (title.isEmpty() || author.isEmpty() || review.isEmpty()) {
+            msgBox.setText("failed submit");
+            msgBox.exec();
+            return;
+        }
+
+        qDebug() << log_form.arg(title).arg(author).arg(review);
+
+        msgBox.setText("success submit");
+        msgBox.exec();
+        clear_inputs();
+    });
+
+    QPushButton reset_button("Reset");
+    hbox.addWidget(&reset_button, 1);
+    QObject::connect(&reset_button, &QPushButton::clicked, [&] {
+        clear_inputs();
+    });
+
+    QPushButton quit_button("Quit");
+    hbox.addWidget(&quit_button, 1);
+    QObject::connect(&quit_button, &QPushButton::clicked, [&] {
+        QApplication::quit();
+    });
 
     w.resize(500 , 500);
     w.show();
