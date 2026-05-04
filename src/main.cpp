535b09c1f2701b1a41d68b71af5ea3e1e7dc4c5f
yareach <mine8345@naver.com>
2026-05-04 17:16:23 +0900

    practice: QRadioButton Widget (wikidocs 5-4)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QCheckBox>
+#include <QtWidgets/QRadioButton>
+#include <QtWidgets/QSlider>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QLabel>
 
@@ -11,39 +13,77 @@ int main(int argc, char* argv[]) {
     QVBoxLayout v_layout(&w);
 
     QString rgb_string("#000000");
-    bool is_r_on = false;
-    bool is_g_on = false;
-    bool is_b_on = false;
 
-    QHBoxLayout h_layout;
-    v_layout.addLayout(&h_layout);
+    QHBoxLayout rgb_layout;
+    v_layout.addLayout(&rgb_layout);
 
     QButtonGroup rgb_group;
     rgb_group.setExclusive(false);
     // r
     QCheckBox r_check_box("r");
-    h_layout.addWidget(&r_check_box);
+    rgb_layout.addWidget(&r_check_box);
     rgb_group.addButton(&r_check_box);
     // g
     QCheckBox g_check_box("g");
-    h_layout.addWidget(&g_check_box);
+    rgb_layout.addWidget(&g_check_box);
     rgb_group.addButton(&g_check_box);
     // b
     QCheckBox b_check_box("b");
-    h_layout.addWidget(&b_check_box);
+    rgb_layout.addWidget(&b_check_box);
     rgb_group.addButton(&b_check_box);
+
+    QHBoxLayout text_h_align_layout;
+    v_layout.addLayout(&text_h_align_layout);
+    Qt::AlignmentFlag h_align_flag = Qt::AlignmentFlag::AlignHCenter;
+
+    QButtonGroup text_h_align_group;
+    QRadioButton h_left("left");
+    text_h_align_group.addButton(&h_left);
+    text_h_align_layout.addWidget(&h_left);
+    QRadioButton h_center("center");
+    text_h_align_group.addButton(&h_center);
+    text_h_align_layout.addWidget(&h_center);
+    QRadioButton h_right("right");
+    text_h_align_group.addButton(&h_right);
+    text_h_align_layout.addWidget(&h_right);
+    h_center.setChecked(true);
+
+    QHBoxLayout text_v_align_layout;
+    v_layout.addLayout(&text_v_align_layout);
+    Qt::AlignmentFlag v_align_flag = Qt::AlignmentFlag::AlignVCenter;
+
+    QButtonGroup text_v_align_group;
+    QRadioButton v_top("top");
+    text_v_align_group.addButton(&v_top);
+    text_v_align_layout.addWidget(&v_top);
+    QRadioButton v_center("center");
+    text_v_align_group.addButton(&v_center);
+    text_v_align_layout.addWidget(&v_center);
+    QRadioButton v_bottom("bottom");
+    text_v_align_group.addButton(&v_bottom);
+    text_v_align_layout.addWidget(&v_bottom);
+    v_center.setChecked(true);
+
+    int font_size = 20;
+    QSlider slider(Qt::Horizontal);
+    slider.setRange(1, 100);
+    slider.setValue(font_size);
+    v_layout.addWidget(&slider);
 
     QLabel label(QString("rgb %1").arg(rgb_string));
     auto update_label_style = [&] {
         const QString new_style(
             "background-color: %1;"
             "color: %2;"
+            "font-size: %3px;"
         );
         label.setStyleSheet(
             new_style
                 .arg(rgb_string)
                 .arg("#777777")
+                .arg(font_size)
         );
+        label.setAlignment(v_align_flag | h_align_flag);
     };
 
     QObject::connect(&rgb_group, &QButtonGroup::buttonToggled, [&](const QAbstractButton* btn, auto checked) {
@@ -64,6 +104,38 @@ int main(int argc, char* argv[]) {
         update_label_style();
         label.setText(QString("rgb %1").arg(rgb_string));
     });
+
+    QObject::connect(&text_h_align_group, &QButtonGroup::buttonToggled, [&](const QAbstractButton* btn, auto checked) {
+        if (&h_left == btn) {
+            h_align_flag = Qt::AlignLeft;
+        } else if (&h_center == btn) {
+            h_align_flag = Qt::AlignHCenter;
+        } else if (&h_right == btn) {
+            h_align_flag = Qt::AlignRight;
+        } else {
+            throw std::runtime_error("Unknown CheckBox is changed");
+        }
+        update_label_style();
+    });
+
+    QObject::connect(&text_v_align_group, &QButtonGroup::buttonToggled, [&](const QAbstractButton* btn, auto checked) {
+        if (&v_top == btn) {
+            v_align_flag = Qt::AlignTop;
+        } else if (&v_center == btn) {
+            v_align_flag = Qt::AlignVCenter;
+        } else if (&v_bottom == btn) {
+            v_align_flag = Qt::AlignBottom;
+        } else {
+            throw std::runtime_error("Unknown CheckBox is changed");
+        }
+        update_label_style();
+    });
+
+    QObject::connect(&slider, &QSlider::valueChanged, [&](int value) {
+        font_size = value;
+        update_label_style();
+    });
+
     update_label_style();
 
     label.setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
