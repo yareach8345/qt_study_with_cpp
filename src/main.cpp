40edbff2bc01f9e60210be30145dd3d79a2e40b3
yareach <mine8345@naver.com>
2026-05-08 14:20:49 +0900

    practice: QSlider & QDial Widget (wikidocs 5-8)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QProgressBar>
 #include <QtWidgets/QLabel>
-#include <QtCore/QTimer>
-#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QSlider>
+#include <QtWidgets/QDial>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QHBoxLayout>
 
 #include "widgets/CustomProgressBar.h"
 
@@ -10,44 +11,74 @@ int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
 
     QWidget w;
-    QVBoxLayout layout(&w);
+    QVBoxLayout vbox(&w);
 
-    QProgressBar pb1;
-    QProgressBar pb2;
-    QProgressBar pb3;
-    QLabel lb("...");
+    std::vector<QSlider*> sbs;
 
-    layout.addWidget(&pb1);
-    layout.addWidget(&pb2);
-    layout.addWidget(&pb3);
-    layout.addWidget(&lb);
-    layout.addStretch(1);
+    QSlider sb1(Qt::Orientation::Horizontal);
+    QSlider sb2(Qt::Orientation::Horizontal);
+    QSlider sb3(Qt::Orientation::Horizontal);
+    QSlider sb4;
 
-    pb1.setMinimum(0);
-    pb1.setMaximum(0);
+    QDial dial;
 
-    pb2.setValue(50);
+    sbs.push_back(&sb1);
+    sbs.push_back(&sb2);
+    sbs.push_back(&sb3);
+    sbs.push_back(&sb4);
 
-    pb3.setOrientation(Qt::Orientation::Vertical);
+    vbox.addWidget(&sb1);
+    sb1.setRange(0, 100);
 
-    QTimer timer;
-    timer.start(100);
+    vbox.addWidget(&sb2);
+    sb2.setTickPosition(QSlider::TickPosition::TicksAbove);
 
+    vbox.addWidget(&sb3);
+    sb3.setTickPosition(QSlider::TickPosition::TicksBelow);
+    sb3.setTickInterval(10);
+
+    vbox.addWidget(&sb4);
+    sb4.setTickPosition(QSlider::TickPosition::TicksRight);
+    sb4.setTickInterval(20);
+
+    vbox.addWidget(&dial);
+    dial.setRange(0, 100);
+
+    QHBoxLayout hbox;
+    vbox.addLayout(&hbox);
+
+    hbox.addWidget(new QLabel("0"), 1);
     CustomProgressBar cp({ 40, 60 });
-    layout.addWidget(&cp);
+    hbox.addWidget(&cp, 20);
+    hbox.addWidget(new QLabel("100"), 1);
+
+    for (QSlider* slider: sbs) { slider->setValue(50); }
+
+    auto slider_update = [&sbs, &dial, &cp](const int value) {
+        for (QSlider* slider : sbs) {
+            slider->setValue(value);
+        }
+        dial.setValue(value);
+        cp.setValue(value);
+    };
+
+    for (const QSlider* slider: sbs) {
+        QObject::connect(slider, &QSlider::valueChanged, slider_update);
+    }
+    QObject::connect(&dial, &QSlider::valueChanged, slider_update);
 
     w.resize(400, 500);
     w.show();
 
-    QObject::connect(&timer, &QTimer::timeout, [&]() {
-        const int current_pb3_value = pb3.value();
-        pb3.setValue(current_pb3_value + 1);
-        cp.setValue(current_pb3_value);
-        if (current_pb3_value == 100) {
-            timer.stop();
-            lb.setText("Finished...");
-        }
-    });
+    // QObject::connect(&timer, &QTimer::timeout, [&]() {
+    //     const int current_pb3_value = pb3.value();
+    //     pb3.setValue(current_pb3_value + 1);
+    //     cp.setValue(current_pb3_value);
+    //     if (current_pb3_value == 100) {
+    //         timer.stop();
+    //         lb.setText("Finished...");
+    //     }
+    // });
 
     return QApplication::exec();
 }
