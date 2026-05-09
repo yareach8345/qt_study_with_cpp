79c906f58d439683f3504579375c059564bb2980
yareach <mine8345@naver.com>
2026-05-10 06:08:11 +0900

    practice: QGroupBox (wikidocs 5-10)

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,102 @@
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QSlider>
-#include <QtWidgets/QHBoxLayout>
-#include <QtWidgets/QSplitter>
-#include "widgets/CustomLabel.h"
+#include <QtWidgets/QRadioButton>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QMenu>
+
+QGroupBox *gen_radio_buttons_group() {
+    auto *group = new QGroupBox("Exclusive Radio Buttons");
+
+    auto *layout = new QVBoxLayout;
+
+    for (int i = 0; i < 3; i++) {
+        const auto new_radio = new QRadioButton(QString("Radio%1").arg(i + 1));
+        layout->addWidget(new_radio);
+        if (i == 0) {
+            new_radio->setChecked(true);
+        }
+    }
+
+    group->setLayout(layout);
+
+    return std::move(group);
+}
+
+QGroupBox *gen_check_boxes_group() {
+    auto *group = new QGroupBox("Non-Exclusive Checkboxes");
+
+    auto *layout = new QVBoxLayout;
+
+    for (int i = 0; i < 3; i++) {
+        const auto new_radio = new QCheckBox(QString("Checkbox%1").arg(i + 1));
+        layout->addWidget(new_radio);
+    }
+    layout->addStretch(1);
+
+    group->setLayout(layout);
+
+    return std::move(group);
+}
+
+QGroupBox *gen_radio_buttons_group2() {
+    auto group = gen_radio_buttons_group();
+    group->setCheckable(true);
+    group->setChecked(true);
+
+    const auto checkbox = new QCheckBox("Independent Checkbox");
+    checkbox->setChecked(true);
+    group->layout()->addWidget(checkbox);
+
+    return std::move(group);
+}
+
+QGroupBox *gen_push_buttons_groups(QWidget* widget) {
+    auto *group = new QGroupBox("Push Buttons");
+    group->setCheckable(true);
+    group->setChecked(true);
+
+    const auto pushbutton = new QPushButton("Normal Button");
+
+    const auto toggleButton = new QPushButton("Toggle Button");
+    toggleButton->setCheckable(true);
+    toggleButton->setChecked(true);
+
+    const auto flatButton = new QPushButton("Flat Button");
+    flatButton->setFlat(true);
+
+    const auto popupButton = new QPushButton("Flat Button");
+    const auto menu = new QMenu(widget);
+    menu->addAction("First Item");
+    menu->addAction("Second Item");
+    menu->addAction("Third Item");
+    menu->addAction("Fourth Item");
+    popupButton->setMenu(menu);
+
+    const auto vBox = new QVBoxLayout;
+    vBox->addWidget(pushbutton);
+    vBox->addWidget(toggleButton);
+    vBox->addWidget(flatButton);
+    vBox->addWidget(popupButton);
+    vBox->addStretch(1);
+
+    group->setLayout(vBox);
+    return std::move(group);
+}
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
 
     QWidget w;
 
-    QVBoxLayout v_layout(&w);
+    QGridLayout layout(&w);
 
-    QSplitter splitter;
-    splitter.setFixedWidth(255);
-    v_layout.addWidget(&splitter);
-
-    CustomLabel r_label("r");
-    r_label.setStyleSheet("background-color: red;");
-
-    CustomLabel g_label("g");
-    g_label.setStyleSheet("background-color: green;");
-
-    CustomLabel b_label("b");
-    b_label.setStyleSheet("background-color: blue;");
-
-    splitter.addWidget(&r_label);
-    splitter.addWidget(&g_label);
-    splitter.addWidget(&b_label);
-
-    QLabel label("#000000");
-    v_layout.addWidget(&label);
-
-    auto update_label_color = [&]() {
-        const int r = r_label.width();
-        const int g = g_label.width();
-        const int b = b_label.width();
-
-        const auto rgb_string = QString("background-color: #%1%2%3;")
-            .arg(r, 2, 16, QChar('0'))
-            .arg(g, 2, 16, QChar('0'))
-            .arg(b, 2, 16, QChar('0'));
-
-        qDebug() << r << g << b << rgb_string;
-        label.setStyleSheet(rgb_string);
-        label.setText(rgb_string);
-    };
-
-    QObject::connect(&r_label, &CustomLabel::resized, [&](const int width){
-        update_label_color();
-    });
-
-    QObject::connect(&g_label, &CustomLabel::resized, [&](const int width){
-        update_label_color();
-    });
-
-    QObject::connect(&b_label, &CustomLabel::resized, [&](const int width){
-        update_label_color();
-    });
+    layout.addWidget(gen_radio_buttons_group(), 0, 0);
+    layout.addWidget(gen_check_boxes_group(), 0, 1);
+    layout.addWidget(gen_radio_buttons_group2(), 1, 0);
+    layout.addWidget(gen_push_buttons_groups(&w), 1, 1);
 
     w.setWindowTitle("Window");
     w.show();
