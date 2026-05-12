b74d891ad3153d1193ce143f3925963c68fb1d35
yareach <mine8345@naver.com>
2026-05-12 12:41:58 +0900

    practice: QPixmap (wikidocs 5-12)

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,74 @@
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QPushButton>
-#include <QtWidgets/QTabWidget>
-#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QLabel>
 #include <QtCore/QtCore>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QSlider>
+#include <QColor>
 
-class TabWidget : public QWidget {
-private:
-    QPushButton btn;
-public:
-    explicit TabWidget(QString name): btn(this) {
-        btn.setText(name);
+QImage blur(const QImage &original, const int d) {
+    QImage blurred(original.width(), original.height(), QImage::Format_RGB32);
 
-        const auto r = QRandomGenerator::global();
-        const int x = r->bounded(0, 200);
-        const int y = r->bounded(0, 200);
-        btn.setGeometry(x, y, 100, 100);
+    const int div = (d * 2 + 1) * (d * 2 + 1);
+
+    for (int x = d; x < original.width() - d; x++) {
+        for (int y = d; y < original.height() - d; y++) {
+            int r = 0;
+            int g = 0;
+            int b = 0;
+            for (int dx = -d; dx <= d; dx++) {
+                for (int dy = -d; dy <= d; dy++) {
+                    auto color = original.pixelColor(x + dx, y + dy);
+                    r += color.red();
+                    g += color.green();
+                    b += color.blue();
+                }
+            }
+            r /= div;
+            g /= div;
+            b /= div;
+
+            blurred.setPixelColor(x, y, QColor(r, g, b));
+        }
     }
 
-};
+    return std::move(blurred);
+}
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
 
     QWidget w;
 
-    auto tabs = QTabWidget();
-    tabs.addTab(new TabWidget("Tab1"), "Tab1");
-    tabs.addTab(new TabWidget("Tab2"), "Tab2");
-    tabs.addTab(new TabWidget("Tab3"), "Tab3");
-    tabs.setTabsClosable(true);
+    QVBoxLayout layout(&w);
 
-    QObject::connect(&tabs, &QTabWidget::tabCloseRequested, [&](auto index) {
-        tabs.removeTab(index);
-    });
+    QImage original_img(":images/profile.jpg");
 
-    QObject::connect(&tabs, &QTabWidget::currentChanged, [&](auto index) {
-        qDebug() << index;
-    });
+    QLabel label;
+    layout.addWidget(&label);
 
+    auto set_label_pixel_img = [&](const int new_value) {
+        const QImage image = blur(original_img, new_value);
+        const QPixmap pixmap = QPixmap::fromImage(image);
+        label.setPixmap(pixmap);
+    };
 
-    QVBoxLayout vbox(&w);
-    vbox.addWidget(&tabs);
+    QSlider slider;
+    slider.setMinimum(0);
+    slider.setMaximum(20);
+    slider.setTickInterval(1);
+    slider.setValue(5);
+    slider.setOrientation(Qt::Horizontal);
+    layout.addWidget(&slider);
 
-    w.setWindowTitle("Window");
+    const auto img_info = QString("original image size - w: %1, h: %2").arg(original_img.width()).arg(original_img.height());
+    QLabel label2(img_info);
+    layout.addWidget(&label2);
+
+    QObject::connect(&slider, &QSlider::valueChanged, &w, set_label_pixel_img);
+    set_label_pixel_img(slider.value());
+
+    w.setWindowTitle("Blur. 느림주의.");
     w.show();
 
     return QApplication::exec();
