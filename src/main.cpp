353d2ba3d5a6d67247339dd15533f8b88e1aefec
yareach <mine8345@naver.com>
2026-05-14 10:08:46 +0900

    practice: QSpinBox (wikidocs 5-14)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QWidget>
-#include <QtWidgets/QCalendarWidget>
+#include <QtWidgets/QSpinBox>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QVBoxLayout>
 
@@ -9,24 +9,37 @@ int main(int argc, char* argv[]) {
 
     QWidget w;
 
-    QCalendarWidget calendar;
-    QLabel label;
+    QVBoxLayout layout(&w);
 
-    calendar.setGridVisible(true);
+    QSpinBox first_number_spinbox;
+    layout.addWidget(&first_number_spinbox);
+    first_number_spinbox.setMinimum(1);
+    first_number_spinbox.setMaximum(9);
+    first_number_spinbox.setValue(5);
 
-    auto update_label = [&label, &calendar] {
-        label.setText(calendar.selectedDate().toString());
+    QSpinBox second_number_spinbox;
+    layout.addWidget(&second_number_spinbox);
+    second_number_spinbox.setMinimum(1);
+    second_number_spinbox.setMaximum(9);
+    second_number_spinbox.setValue(5);
+
+    QLabel result_label;
+    layout.addWidget(&result_label);
+    const QString result_template = "%1 * %2 = %3";
+    auto result_label_update = [&] {
+        const int first_number = first_number_spinbox.value();
+        const int second_number = second_number_spinbox.value();
+        const int result = first_number * second_number;
+        result_label.setText(result_template.arg(first_number).arg(second_number).arg(result));
     };
-    update_label();
+    result_label_update();
 
-    QObject::connect(&calendar, &QCalendarWidget::clicked, update_label);
+    QObject::connect(&first_number_spinbox, &QSpinBox::valueChanged, result_label_update);
 
-    QVBoxLayout layout;
-    layout.addWidget(&calendar);
-    layout.addWidget(&label);
+    QObject::connect(&second_number_spinbox, &QSpinBox::valueChanged, result_label_update);
 
-    w.setLayout(&layout);
-    w.setWindowTitle("QCalendarWidget");
+    w.setWindowTitle("QGugu?");
+    w.setFixedSize(500, 500);
     w.show();
 
     return QApplication::exec();
