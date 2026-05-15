fb339f517b0630dc67f7b7054cc3ccf463209320
yareach <mine8345@naver.com>
2026-05-15 10:17:03 +0900

    practice: QDoubleSpinBox (wikidocs 5-15)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QWidget>
-#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QDoubleSpinBox>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QVBoxLayout>
+
+QDoubleSpinBox* gen_spin_box() {
+    const auto spin_box = new QDoubleSpinBox();
+    spin_box->setRange(0, 10);
+    spin_box->setSingleStep(0.1);
+    spin_box->setValue(5);
+    return spin_box;
+}
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
@@ -11,32 +19,25 @@ int main(int argc, char* argv[]) {
 
     QVBoxLayout layout(&w);
 
-    QSpinBox first_number_spinbox;
-    layout.addWidget(&first_number_spinbox);
-    first_number_spinbox.setMinimum(1);
-    first_number_spinbox.setMaximum(9);
-    first_number_spinbox.setValue(5);
-
-    QSpinBox second_number_spinbox;
-    layout.addWidget(&second_number_spinbox);
-    second_number_spinbox.setMinimum(1);
-    second_number_spinbox.setMaximum(9);
-    second_number_spinbox.setValue(5);
+    const auto first_spinbox = gen_spin_box();
+    layout.addWidget(first_spinbox);
+    const auto second_spinbox = gen_spin_box();
+    layout.addWidget(second_spinbox);
 
     QLabel result_label;
     layout.addWidget(&result_label);
     const QString result_template = "%1 * %2 = %3";
     auto result_label_update = [&] {
-        const int first_number = first_number_spinbox.value();
-        const int second_number = second_number_spinbox.value();
-        const int result = first_number * second_number;
+        const double first_number = first_spinbox->value();
+        const double second_number = second_spinbox->value();
+        const double result = first_number * second_number;
         result_label.setText(result_template.arg(first_number).arg(second_number).arg(result));
     };
     result_label_update();
 
-    QObject::connect(&first_number_spinbox, &QSpinBox::valueChanged, result_label_update);
+    QObject::connect(first_spinbox, &QDoubleSpinBox::valueChanged, result_label_update);
 
-    QObject::connect(&second_number_spinbox, &QSpinBox::valueChanged, result_label_update);
+    QObject::connect(second_spinbox, &QDoubleSpinBox::valueChanged, result_label_update);
 
     w.setWindowTitle("QGugu?");
     w.setFixedSize(500, 500);
