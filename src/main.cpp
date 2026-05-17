999a5b85c28e3dbea052299a0f80e7cef2bc2a5a
yareach <mine8345@naver.com>
2026-05-17 11:09:46 +0900

    practice: QTimeEdit (wikidocs 5-17(py))

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QDateEdit>
+#include <QtWidgets/QTimeEdit>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QVBoxLayout>
-#include <QtCore/QDate>
+#include <QtCore/QTime>
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
@@ -11,19 +11,19 @@ int main(int argc, char* argv[]) {
 
     QVBoxLayout layout(&w);
 
-    layout.addWidget(new QLabel("QDateEdit"));
+    layout.addWidget(new QLabel("QTimeEdit"));
 
-    QDateEdit dateEdit;
-    layout.addWidget(&dateEdit);
-    dateEdit.setDate(QDate::currentDate());
-    dateEdit.setMinimumDate(QDate(2001, 01, 01));
-    dateEdit.setMaximumDate(QDate(2099, 12, 31));
+    QTimeEdit timeEdit;
+    layout.addWidget(&timeEdit);
+    timeEdit.setTime(QTime::currentTime());
+    timeEdit.setMinimumTime(QTime(00, 00, 00));
+    timeEdit.setMaximumTime(QTime(24, 00, 00));
 
-    QObject::connect(&dateEdit, &QDateEdit::dateChanged, [](const QDate new_date) {
+    QObject::connect(&timeEdit, &QTimeEdit::dateChanged, [](const QDate new_date) {
         qDebug() << new_date.toString();
     });
 
-    w.setWindowTitle("QGugu?");
+    w.setWindowTitle("QTime?");
     w.setFixedSize(500, 500);
     w.show();
 
