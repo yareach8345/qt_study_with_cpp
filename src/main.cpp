0a98de13506a869621a3b2752bf0fa8c758d8314
yareach <mine8345@naver.com>
2026-05-19 15:30:08 +0900

    practice: QTextBrowser (wikidocs 5-19(py))

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,54 @@
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QDateTimeEdit>
-#include <QtWidgets/QLabel>
-#include <QtWidgets/QVBoxLayout>
-#include <QtCore/QDateTime>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QTextBrowser>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPushButton>
+
+class Edit: QWidget {
+private:
+    QVBoxLayout layout;
+    QHBoxLayout input_layout;
+    QLineEdit line_edit;
+    QPushButton add_button;
+    QPushButton clear_button;
+    QTextBrowser text_browser;
+
+    void add_button_clicked() {
+        const QString& input = line_edit.text();
+        text_browser.append(input);
+    }
+
+    void clear_button_clicked() {
+        text_browser.clear();
+    }
+public:
+    explicit Edit(QWidget *parent) :
+        QWidget(parent) ,
+        layout(parent),
+        add_button("add"),
+        clear_button("clear")
+    {
+        input_layout.addWidget(&line_edit);
+        input_layout.addWidget(&add_button);
+        layout.addLayout(&input_layout);
+        layout.addWidget(&text_browser);
+        text_browser.setAcceptRichText(true);
+        text_browser.setOpenExternalLinks(true);
+        layout.addWidget(&clear_button);
+
+        QObject::connect(&add_button, &QPushButton::clicked, [this] { this->add_button_clicked(); });
+        QObject::connect(&clear_button , &QPushButton::clicked, [this]{ this->clear_button_clicked(); });
+    }
+};
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
 
     QWidget w;
 
-    QVBoxLayout layout(&w);
+    Edit edit(&w);
 
-    layout.addWidget(new QLabel("QTimeEdit"));
-
-    QDateTimeEdit datetimeEdit;
-    layout.addWidget(&datetimeEdit);
-    datetimeEdit.setDateTime(QDateTime::currentDateTime());
-    datetimeEdit.setMinimumDateTime(QDateTime(QDate(2000, 1, 1), QTime(0, 0, 0)));
-    datetimeEdit.setMaximumDateTime(QDateTime(QDate(2099, 12, 31), QTime(23, 59, 59)));
-
-    QLabel label;
-    layout.addWidget(&label);
-    auto update_label = [&label](const QDateTime& date_time) {
-        label.setText(date_time.toString("yyyy년 MM월 dd일 hh시 mm분 ss초"));
-    };
-    update_label(datetimeEdit.dateTime());
-
-    QObject::connect(&datetimeEdit, &QDateTimeEdit::dateTimeChanged, [&update_label](const QDateTime &date_time) {
-        qDebug() << "qDateTime updated to " << date_time.toString();
-        update_label(date_time);
-    });
-
-    w.setWindowTitle("QTime?");
+    w.setWindowTitle("edit?");
     w.setFixedSize(500, 500);
     w.show();
 
