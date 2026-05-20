153924cb7705ee9b55f9cc11efc95eb3b295053b
yareach <mine8345@naver.com>
2026-05-20 16:13:29 +0900

    practice: QTextEdit (wikidocs 5-20(py))

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,19 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QTextEdit>
 #include <QtWidgets/QTextBrowser>
-#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QPushButton>
 
 class Edit: QWidget {
 private:
     QVBoxLayout layout;
     QHBoxLayout input_layout;
-    QLineEdit line_edit;
-    QPushButton add_button;
+    QTextEdit text_edit;
     QPushButton clear_button;
     QTextBrowser text_browser;
 
-    void add_button_clicked() {
-        const QString& input = line_edit.text();
-        text_browser.append(input);
+    void text_edit_updated() {
+        text_browser.setMarkdown(text_edit.toPlainText());
     }
 
     void clear_button_clicked() {
@@ -25,19 +23,17 @@ public:
     explicit Edit(QWidget *parent) :
         QWidget(parent) ,
         layout(parent),
-        add_button("add"),
         clear_button("clear")
     {
-        input_layout.addWidget(&line_edit);
-        input_layout.addWidget(&add_button);
+        input_layout.addWidget(&text_edit);
+        input_layout.addWidget(&text_browser);
         layout.addLayout(&input_layout);
-        layout.addWidget(&text_browser);
         text_browser.setAcceptRichText(true);
         text_browser.setOpenExternalLinks(true);
         layout.addWidget(&clear_button);
 
-        QObject::connect(&add_button, &QPushButton::clicked, [this] { this->add_button_clicked(); });
         QObject::connect(&clear_button , &QPushButton::clicked, [this]{ this->clear_button_clicked(); });
+        QObject::connect(&text_edit, &QTextEdit::textChanged, [this]{ text_edit_updated(); });
     }
 };
 
@@ -48,7 +44,7 @@ int main(int argc, char* argv[]) {
 
     Edit edit(&w);
 
-    w.setWindowTitle("edit?");
+    w.setWindowTitle("markdown edit?");
     w.setFixedSize(500, 500);
     w.show();
 
