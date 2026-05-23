114bf413b0593359455f08f386a4da204347eeda
yareach <mine8345@naver.com>
2026-05-24 06:15:33 +0900

    practice: QFontDialog (wikidocs 6-3(py))

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,43 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QVBoxLayout>
-#include <QtWidgets/QHBoxLayout>
-#include "widgets/ColorInput.h"
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QFontDialog>
 
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
 
     QWidget w;
-
     QVBoxLayout layout(&w);
     layout.setAlignment(Qt::AlignHCenter);
 
-    QHBoxLayout color_inputs;
-    layout.addLayout(&color_inputs);
+    QTextEdit edit;
+    edit.setPlaceholderText("Input text");
+    layout.addWidget(&edit, 1);
 
-    ColorInput color_input1(QColor(255, 0, 0));
-    ColorInput color_input2(QColor(0, 0, 255));
-    color_inputs.addWidget(&color_input1);
-    color_inputs.addWidget(&color_input2);
+    QLabel label;
+    label.setAlignment(Qt::AlignHCenter);
+    layout.addWidget(&label, 1);
 
-    QLabel l1("混ぜたら 混ぜたら 何色 なるかな");
-    l1.setAlignment(Qt::AlignHCenter);
-    layout.addWidget(&l1);
+    QPushButton button("Select font");
+    layout.addWidget(&button, 0);
 
-    ColorViewer result_color;
-    auto update_result = [&]() {
-        const QColor color1 = color_input1.color();
-        const QColor color2 = color_input2.color();
-        const auto result = QColor(
-            (color1.red() + color2.red()) / 2,
-            (color1.green() + color2.green()) / 2,
-            (color1.blue() + color2.blue()) / 2
-        );
-        qDebug() << color1.name();
-        qDebug() << color2.name();
-        qDebug() << result.name();
+    QObject::connect(&edit, &QTextEdit::textChanged, [&]() {
+        label.setText(edit.toPlainText());
+    });
 
-        result_color.setColor(&result);
-    };
-    update_result();
-    layout.addWidget(&result_color);
+    QObject::connect(&button, &QPushButton::clicked, [&]() {
+        bool ok_flag;
+        const auto selected_font = QFontDialog::getFont(&ok_flag, &w);
 
-    QLabel l2("STAMP!");
-    l2.setAlignment(Qt::AlignHCenter);
-    layout.addWidget(&l2);
+        if (ok_flag == false) { return; }
 
-    QObject::connect(&color_input1, &ColorInput::colorChanged, [&] { update_result(); });
-    QObject::connect(&color_input2, &ColorInput::colorChanged, [&] { update_result(); });
+        label.setFont(selected_font);
+    });
 
-    w.setWindowTitle("Mazetara");
+    w.setWindowTitle("font");
     w.setMinimumSize(500, 500);
     w.show();
 
