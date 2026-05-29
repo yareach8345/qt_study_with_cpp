4210c5ec7ac40aa0c32e16b185876e9d257b81e0
yareach <mine8345@naver.com>
2026-05-30 07:26:26 +0900

    practice: reconstruct event handler (wikidocs 7-4(py))

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QDial>
 #include <QKeyEvent>
+#include <QPoint>
+#include <QtWidgets/QLabel>
 
 class MyApp : public QWidget {
 private:
+    static QString label_style_template;
+    static QString color_table[];
+
+    static QString mouse_info_template;
+
+    int double_click_count = 0;
+
+    QLabel mouse_point_label;
+
     void initUi() {
+        mouse_point_label.setFixedWidth(100);
+        mouse_point_label.setWordWrap(true);
         setWindowTitle("Reimplementing event handler");
-        setGeometry(300, 300, 300, 200);
+        setGeometry(300, 300, 500, 500);
         show();
     }
 
@@ -27,10 +39,33 @@ protected:
         }
     }
 
+    void mouseMoveEvent(QMouseEvent* event) override {
+        const auto pos = event->pos();
+        mouse_point_label.setText(mouse_info_template.arg(pos.x()).arg(pos.y()));
+        mouse_point_label.move(pos);
+    }
+
+    void mouseDoubleClickEvent(QMouseEvent* event) override {
+        double_click_count = ++double_click_count & 0b011;
+        mouse_point_label.setStyleSheet(label_style_template.arg(color_table[double_click_count]));
+    }
+
 public:
-    MyApp() : QWidget() {
+    MyApp() : QWidget(), mouse_point_label(this) {
+        setMouseTracking(true);
         initUi();
     }
+};
+
+QString MyApp::mouse_info_template("x: %1, y: %2");
+
+QString MyApp::label_style_template("background-color: %1;");
+
+QString MyApp::color_table[] = {
+    "#ffffff",
+    "#ff0000",
+    "#00ff00",
+    "#0000ff",
 };
 
 int main(int argc, char* argv[]) {
